3aaf9e4cd8ed3e62ac27d9e2ec56d7f0d3654902
VaseHaya <113222402+VaseHaya@users.noreply.github.com>
2025-10-25 03:42:26 +0800

    10-25 22.作业练习

---
+++ 08_LoadTextureExercise/shader/fragment.glsl
@@ -0,0 +1,14 @@
+#version 330 core
+out vec4 FragColor;
+in vec3 ourColor;
+in vec2 texCoord;
+
+// 采样
+uniform sampler2D texture1; // 默认传递为 0
+uniform sampler2D texture2;
+
+void main()
+{
+
+    FragColor = mix(texture(texture1, texCoord), texture(texture2, texCoord), 0.2);
+}
